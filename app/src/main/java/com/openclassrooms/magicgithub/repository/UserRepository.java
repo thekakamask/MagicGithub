db5e4541e431b5525cf11e46cb8b47f0e33ba978
thekakamask <59416432+thekakamask@users.noreply.github.com>
2020-09-10 00:41:15 +0200

    Update UserRepository.java

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -14,17 +14,14 @@ public class UserRepository {
     }
 
     public List<User> getUsers() {
-        // TODO: A modifier
         return apiService.getUsers();
     }
 
     public void generateRandomUser() {
-        // TODO: A modifier
         apiService.generateRandomUser();
     }
 
     public void deleteUser(User user) {
-        // TODO: A modifier
         apiService.deleteUser(user);
     }
 }
